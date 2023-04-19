44f2366e344e6cd2d019426a95d61f8a894965f2
victoriacassolo <vitoria.cassolo@gmail.com>
2023-04-19 14:51:03 -0300

    feat: add events to connections

--- app/pages/_shared/constants.js
+++ app/pages/_shared/constants.js
@@ -8,5 +8,6 @@ export const constants = {
     USER_CONNECTED: 'userConnection',
     USER_DISCONNECTED: 'userDisconnection',
     JOIN_ROOM: 'joinRoom',
+    LOBBY_UPDATED: 'lobbyUpdated',
   },
 };
--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,7 +1,7 @@
 import { constants } from '../../_shared/constants.js';
-import SocketBuilder from '../../_shared/socketBuilder.js';
+import RoomSocketBuilder from './util/roomSocket.js';
 
-const socketBuilder = new SocketBuilder({
+const socketBuilder = new RoomSocketBuilder({
   socketUrl: constants.socketUrl,
   namespace: constants.socketNamespaces.room,
 });
@@ -9,16 +9,17 @@ const socketBuilder = new SocketBuilder({
 const socket = socketBuilder
   .setOnUserConnected((user) => console.log('user connected', user))
   .setOnUserDisconnected((user) => console.log('user disconnected', user))
+  .setOnRoomUpdated((room) => console.log('room list!', room))
   .build();
 
 const room = {
-  id: Date.now(),
+  id: '0001',
   topic: 'JS Expert test room',
 };
 
 const user = {
   img: 'https://pics.freeicons.io/uploads/icons/png/6822363841598811069-512.png',
-  username: 'Victoria Cassolo',
+  username: 'Victoria Cassolo ' + Date.now(),
 };
 
 socket.emit(constants.events.JOIN_ROOM, { user, room });
